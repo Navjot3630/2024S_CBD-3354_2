de713f87298c7cde7f5847a28466cb7b016cab8c
Navjot Singh <105104943+Navjot3630@users.noreply.github.com>
2024-07-15 21:35:06 -0500

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,12 +1,13 @@
 provider "google" {
   project = var.project_id
   region  = var.region
-  # serviceAccount is not used here
+  # Credentials should be provided by setting the GOOGLE_APPLICATION_CREDENTIALS environment variable
+  # service_account = "gke-cluster-admin@project897927.iam.gserviceaccount.com"
 }
 
 resource "google_container_cluster" "primary" {
-  name     = var.cluster_name
-  location = var.region
+  name               = var.cluster_name
+  location           = var.region
   initial_node_count = var.node_count
 
   node_config {
@@ -15,23 +16,22 @@ resource "google_container_cluster" "primary" {
     oauth_scopes    = ["https://www.googleapis.com/auth/cloud-platform"]
   }
 
-  # Enable or disable Autopilot Mode
-  enable_autopilot = false  # Set to true if you want to use Autopilot Mode
-
-  # Configure Cluster Autoscaler
+  # Cluster Autoscaler Configuration
   cluster_autoscaling {
     enable_cluster_autoscaler = true
-    min_node_count = 1
-    max_node_count = 5
+    # Node pool autoscaling
+    autoscaling {
+      min_node_count = 1
+      max_node_count = 5
+    }
   }
 
-  # Configure Network Policy
+  # Network Policy Configuration
   network_policy {
     enabled = true
   }
 }
 
-# Define a separate node pool for additional configurations (if needed)
 resource "google_container_node_pool" "default" {
   name               = "${google_container_cluster.primary.name}-node-pool"
   location           = var.region
@@ -44,7 +44,7 @@ resource "google_container_node_pool" "default" {
     oauth_scopes    = ["https://www.googleapis.com/auth/cloud-platform"]
   }
 
-  # Configure Node Pool Autoscaling
+  # Node Pool Autoscaler Configuration
   autoscaling {
     min_node_count = 1
     max_node_count = 5
