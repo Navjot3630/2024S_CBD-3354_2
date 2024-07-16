e7312a0b8fec505ec1f193a796b5868a702eee83
Navjot Singh <105104943+Navjot3630@users.noreply.github.com>
2024-07-15 21:37:46 -0500

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,7 @@
 provider "google" {
   project = var.project_id
   region  = var.region
-  # Credentials should be provided by setting the GOOGLE_APPLICATION_CREDENTIALS environment variable
+  # Ensure that the GOOGLE_APPLICATION_CREDENTIALS environment variable is set correctly
   # service_account = "gke-cluster-admin@project897927.iam.gserviceaccount.com"
 }
 
@@ -11,18 +11,33 @@ resource "google_container_cluster" "primary" {
   initial_node_count = var.node_count
 
   node_config {
-    machine_type   = var.machine_type
+    machine_type    = var.machine_type
     service_account = "gke-cluster-admin@project897927.iam.gserviceaccount.com"
-    oauth_scopes    = ["https://www.googleapis.com/auth/cloud-platform"]
+    oauth_scopes     = ["https://www.googleapis.com/auth/cloud-platform"]
   }
 
-  # Cluster Autoscaler Configuration
+  # Corrected Cluster Autoscaler Configuration
   cluster_autoscaling {
     enable_cluster_autoscaler = true
-    # Node pool autoscaling
-    autoscaling {
-      min_node_count = 1
-      max_node_count = 5
+    autoscaling_profile      = "BALANCED"
+
+    # Node Pool Autoscaler Configuration
+    resource "google_container_node_pool" "default" {
+      name               = "${google_container_cluster.primary.name}-node-pool"
+      location           = var.region
+      cluster            = google_container_cluster.primary.name
+      initial_node_count = var.node_count
+
+      node_config {
+        machine_type    = var.machine_type
+        service_account = "gke-cluster-admin@project897927.iam.gserviceaccount.com"
+        oauth_scopes     = ["https://www.googleapis.com/auth/cloud-platform"]
+      }
+
+      autoscaling {
+        min_node_count = 1
+        max_node_count = 5
+      }
     }
   }
 
@@ -39,9 +54,9 @@ resource "google_container_node_pool" "default" {
   initial_node_count = var.node_count
 
   node_config {
-    machine_type   = var.machine_type
+    machine_type    = var.machine_type
     service_account = "gke-cluster-admin@project897927.iam.gserviceaccount.com"
-    oauth_scopes    = ["https://www.googleapis.com/auth/cloud-platform"]
+    oauth_scopes     = ["https://www.googleapis.com/auth/cloud-platform"]
   }
 
   # Node Pool Autoscaler Configuration
