2b2da32b4c67e0edffc005fb0ce1c6e52d7b974e
Navjot Singh <105104943+Navjot3630@users.noreply.github.com>
2024-07-15 21:25:40 -0500

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,34 +1,24 @@
 variable "project_id" {
-  description = "The ID of the Google Cloud project"
-  type        = string
+  description = "The project ID to deploy resources into"
+  default     = "project897927"
 }
 
 variable "region" {
-  description = "The region where the GKE cluster will be created"
-  type        = string
+  description = "The region to deploy resources into"
   default     = "us-central1"
 }
 
 variable "cluster_name" {
   description = "The name of the GKE cluster"
-  type        = string
   default     = "navicluster"
 }
 
 variable "node_count" {
-  description = "Number of initial nodes in the GKE cluster"
-  type        = number
-  default     = 3
+  description = "The number of nodes in the cluster"
+  default     = 1
 }
 
 variable "machine_type" {
-  description = "The type of machine to use for the nodes in the GKE cluster"
-  type        = string
+  description = "The machine type to use for the cluster"
   default     = "e2-medium"
-}
-
-variable "preemptible" {
-  description = "Whether to use preemptible VMs for the node pool"
-  type        = bool
-  default     = true
 }
